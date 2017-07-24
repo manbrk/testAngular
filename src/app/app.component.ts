d63f95241e26300d0df14d033a50ac574ed5c4fe
manbrk <manbrk@gmail.com>
2017-07-24 19:23:12 +0300

    simple adaptive html markup via Material Design Lite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // for placeholder auto-repeat
+  arr = Array;
+  num: number = 9;
+
+  title = 'Deluxe Beauty Shop';
+  image = 'http://via.placeholder.com/200x200';
+
+  menu = [
+    {
+      menuName: 'Beauty Menu 1',
+      menuIcon: 'star_border'
+    },
+    {
+      menuName: 'Beauty Menu 2',
+      menuIcon: 'star_border'
+    },
+    {
+      menuName: 'Beauty Menu 3',
+      menuIcon: 'star'
+    }
+  ];
 }
