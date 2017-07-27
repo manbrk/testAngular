e3aea1c45d04ae03ae9ccef301229d70cd763124
manbrk <manbrk@gmail.com>
2017-07-27 23:57:15 +0300

    added child routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,12 +1,14 @@
 import {Routes} from '@angular/router';
 import {FirstTaskComponent} from './first-task/first-task.component';
 import {SecondTaskComponent} from './second-task/second-task.component';
-import {EditListComponent} from "./second-task/edit-list/edit-list.component";
+import {EditListComponent} from './second-task/edit-list/edit-list.component';
 
 export const appRoutes: Routes = [
   { path: 'first', component: FirstTaskComponent },
-  { path: 'list', component: SecondTaskComponent },
-  { path: 'list/add', component: SecondTaskComponent },
-  { path: 'list/edit/:id', component: EditListComponent },
+  { path: 'list', component: SecondTaskComponent, children: [
+    { path: 'add', component: SecondTaskComponent },
+    { path: 'edit/:id', component: EditListComponent }
+  ] },
+
   { path: '', redirectTo: '/first', pathMatch: 'full' }
 ];
--- src/app/second-task/edit-list/edit-list.component.ts
+++ src/app/second-task/edit-list/edit-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {StorageService} from '../storage.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-edit-list',
@@ -12,12 +12,21 @@ export class EditListComponent implements OnInit {
   itemName: string;
 
   constructor(private storageService: StorageService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute ) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.item = this.storageService.getItem(id);
     this.itemName = this.item.name;
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.item = this.storageService.getItem(+params['id']);
+        }
+      );
   }
 
+  onEdit() {
+
+  }
 }
