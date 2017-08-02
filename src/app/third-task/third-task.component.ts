0097f42621537e2c87bf601ef658a7af18f407b4
manbrk <manbrk@gmail.com>
2017-08-03 00:13:48 +0300

    add third-task table template

--- src/app/third-task/third-task.component.ts
+++ src/app/third-task/third-task.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./third-task.component.css']
 })
 export class ThirdTaskComponent implements OnInit {
+  coordinates = [
+    {x: 1, y: 6},
+    {x: 2, y: 7},
+    {x: 3, y: 8},
+    {x: 4, y: 9},
+    {x: 5, y: 10},
+  ];
 
   constructor() { }
 
