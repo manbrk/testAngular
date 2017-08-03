8777e20dfe8b4546b28d24fa6457dc96f0861266
manbrk <manbrk@gmail.com>
2017-08-03 21:02:21 +0300

    coordinates generator for tables

--- src/app/third-task/third-task.component.ts
+++ src/app/third-task/third-task.component.ts
@@ -6,17 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./third-task.component.css']
 })
 export class ThirdTaskComponent implements OnInit {
-  coordinates = [
-    {x: 1, y: 6},
-    {x: 2, y: 7},
-    {x: 3, y: 8},
-    {x: 4, y: 9},
-    {x: 5, y: 10},
+  table1 = [
+    // {x: 1, y: 6},
+    // {x: 2, y: 7},
+    // {x: 3, y: 8},
+    // {x: 4, y: 9},
+    // {x: 5, y: 10},
   ];
+
+  table2 = [];
+
+  evenNum = [2, 4, 6, 8, 10];
 
   constructor() { }
 
   ngOnInit() {
+    this.getRandomNoDoubles(this.evenNum);
+
+    for (let i = 0; i < 5; i++) {
+      this.table1.push({
+        x: this.getRandom(1, 10),
+        y: this.getRandom(1, 10)
+      });
+      this.table2.push({
+        x: this.evenNum[i],
+        y: this.getRandom(1, 10)
+      });
+    }
   }
+
+  getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getRandomNoDoubles(array) {
+    let i = array.length,
+        j = 0,
+        temp;
+
+    while (i--) {
+      j = Math.floor(Math.random() * (i + 1));
+
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+
+    return array;
+  }
+
+
 
 }
